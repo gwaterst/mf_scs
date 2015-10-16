bd4bc9d988adedc76e909afd2d99e36813981f3c
Steven Diamond <stevend2@stanford.edu>
2015-10-16 12:59:01 -0700

    made verbose show M values

--- linsys/indirect/private.c
+++ linsys/indirect/private.c
@@ -427,8 +427,10 @@ void getPreconditioner(Data *d, Priv *p) {
 	// Py_DECREF(arglist);
 
 	// Debug.
-	printf("M[0]=%f\n", M[0]);
-	printf("M average = %f\n", avg);
+	if (d->VERBOSE) {
+		printf("M[0]=%f\n", M[0]);
+		printf("M average = %f\n", avg);
+	}
 
 #ifdef EXTRAVERBOSE
 	scs_printf("finished getting pre-conditioner\n");
